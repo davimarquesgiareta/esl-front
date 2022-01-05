c242d4f0869da0a733ec3d3afd402bb137686949
Davi Marques <davimgcb@gmail.com>
2022-01-04 21:44:01 -0300

    testando o history

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -25,7 +25,7 @@ export default function Login() {
           <input type="password" class="form-control" name="password" placeholder="Digite sua senha" onChange={e => setPassword(e.target.value)}/>
         </div>
         <div class="d-flex justify-content-end align-items-center mb-3">
-          <a href='/register' for="exampleInputPassword1" class="mr-3 ">Criar uma conta</a>
+          <a style={{color:"blue", cursor:"pointer"}} onClick={()=> history.push('/register')} for="exampleInputPassword1" class="mr-3 ">Criar uma conta</a>
           <button type="button" class="btn btn-primary w-25" onClick={()=> handleLogin({email, password})}>Entrar</button>
         </div>
       </div>    
