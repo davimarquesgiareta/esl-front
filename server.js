567bea5b060aa966e1900ed0d10fcb124639b941
Davi Marques <davimgcb@gmail.com>
2022-01-04 21:32:32 -0300

    voltando ao normal

--- server.js
+++ server.js
@@ -3,26 +3,13 @@ import { resolve } from "path"
 
 const app = express()
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use('/', express.static(
+
+
+app.use('/', express.static(
   
     './build'
   )
 )
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
-// app.use('/', express.static(
-  
-//     './build'
-//   )
-// )
 
 app.listen(process.env.PORT || 3000, (err)=>{
   if (err){ return console.log(err)}
